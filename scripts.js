6753861ca72d230c5b4e1016249dbb6673058d1d
Andy Fan <122738985+ThomasPWiseau@users.noreply.github.com>
2023-02-26 15:00:26 -0500

    Add buttons and playRound functionality

--- scripts.js
+++ scripts.js
@@ -15,37 +15,27 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     let lowerPS = playerSelection.toLowerCase();
     let lowerCS = computerSelection.toLowerCase();
+    const result = document.querySelector('.result');
+    let resultText = '';
     switch(lowerPS){
         case "rock":
-            console.log(lowerCS === lowerPS ? "Tie!": lowerCS === "scissors" ? "You Win! Rock beats Scissors." : "You lose! Rock loses to Paper.");
+            result.innerHTML = (lowerCS === lowerPS ? "Tie!": lowerCS === "scissors" ? "You Win! Rock beats Scissors." : "You lose! Rock loses to Paper.");
             return(lowerCS === lowerPS ? 0 : lowerCS === "scissors" ? 1 : -1);
         case "paper":
-            console.log(lowerCS === lowerPS ? "Tie!" : lowerCS === "rock" ? "You Win! Paper beats Rock." : "You lose! Paper loses to Scissors.");
+            result.innerHTML = (lowerCS === lowerPS ? "Tie!" : lowerCS === "rock" ? "You Win! Paper beats Rock." : "You lose! Paper loses to Scissors.");
             return(lowerCS === lowerPS ? 0 : lowerCS === "rock" ? 1 : -1);
         case "scissors":
-            console.log(lowerCS === lowerPS ? "Tie!" : lowerCS === "paper" ? "You Win! Scissors beats Paper." : "You lose! Scissors loses to Rock.");
+            result.innerHTML = (lowerCS === lowerPS ? "Tie!" : lowerCS === "paper" ? "You Win! Scissors beats Paper." : "You lose! Scissors loses to Rock.");
             return(lowerCS === lowerPS ? 0 : lowerCS === "paper" ? 1 : -1);
         default:
-            console.log("Please enter a valid section of either Rock, Paper, or Scissors.")
+            result.innerHTML = "Please enter a valid section of either Rock, Paper, or Scissors.";
     }
 }
 
 function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Please enter your choice of Rock, Paper or Scissors");
-        let computerSelection = getComputerChoice();
-        let result = playRound(playerSelection,computerSelection);
-        console.log("Result is..." + result);
-        if(result == 1){
-            playerScore++;
-        }else if(result == -1){
-            computerScore++;
-        }
-        console.log("Current Score. You:" + playerScore + ", Computer Score: " + computerScore);
-    }
-    console.log("Final Score. You:" + playerScore + ", Computer Score: " + computerScore);
+    
 }
 
-game();
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', () => playRound(button.innerHTML,getComputerChoice())));
